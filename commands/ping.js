f64fe6c2911926827a860c27cfa6354b91ee77ff
arizl <imariuwu@gmail.com>
2022-01-25 10:04:01 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "ping",
     description: "this is a ping command",
     execute(message,args){
-        message.channel.send('');
+        message.channel.send(`pong, ${Date.now() - message.createdTimestamp}ms`);
     }
 }
