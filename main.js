4e23beb3f7c6f74e034d34b27af49294111eb722
arizl <imariuwu@gmail.com>
2022-02-01 17:37:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -140,12 +140,11 @@ client.on("messageCreate",message=>{
         let changelogschannel = "936126466104623215"
         if(message.member.roles.cache.some(role => role.name === 'segs changelog')){
             message.channel.send('you already have access to the changelogs channel!')
-        }
-        
+        } else {
         let role = message.member.guild.roles.cache.find(role => role.name === "segs changelog");
         if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
         message.channel.send('you now have access to changelogs channel!')
     }
-});
+};
 
 client.login('OTM0NzkzNTE1MTY5Mzc0MjQ5.Ye1QTQ.tcj1aeQMaOecpR2Fsc5XmSLsJKw');
