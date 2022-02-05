a709299a2b193513c4345db5c4ed4a658a12b20f
arizl <imariuwu@gmail.com>
2022-02-05 20:37:56 +0700

    Update spamle.js

--- commands/spamle.js
+++ commands/spamle.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "spamle",
-    description: "help menu",
+    description: "spam mf le kek",
     execute(client,message,args,prefix){
         if (message.author.bot) return;
         var allowedids = ["840145281806368798","732043268946133133","480288826187710464"]
-        if (allowedids.includes(message.author.id)){
+        if (!allowedids.includes(message.author.id)){
             message.channel.send('mày làm gì có tuổi ping <:lul:806387931606024232>')
         } else {
 
