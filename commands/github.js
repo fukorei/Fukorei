ae7cf93da9b0a8dff7a9bde331df1a92e860078c
arizl <imariuwu@gmail.com>
2022-02-05 22:55:27 +0700

    finishing github commands

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ module.exports = {
         if(args[0] === 'commits'){
             message.channel.send('here are the commits, basiclly whats changed in my code\n<https://github.com/arizl/segs/commits/main>')
         } else if(args[0] === 'main'){
-            message.channel.send('here is the main.js file:<https://github.com/arizl/segs/blob/main/main.js>')
+            message.channel.send('here is the main.js file:\n<https://github.com/arizl/segs/blob/main/main.js>')
         } else if(args[0] === 'commands'){
             message.channel.send('here are the commands .js files:\n<https://github.com/arizl/segs/tree/main/commands>')
         } else if (!args[0]){
