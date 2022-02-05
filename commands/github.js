06740e3381e5107ec2d4c6ad82af8c83727efdf7
arizl <imariuwu@gmail.com>
2022-02-05 22:56:02 +0700

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -2,7 +2,6 @@ module.exports = {
     name: "github",
     description: "github commands",
     execute(client,message,args,prefix){
-        console.log(args)
         if(args[0] === 'commits'){
             message.channel.send('here are the commits, basiclly whats changed in my code\n<https://github.com/arizl/segs/commits/main>')
         } else if(args[0] === 'main'){
