d3adf682005812ef6cfc6b4d3f9a0578200b86e7
arizl <imariuwu@gmail.com>
2022-02-05 22:54:11 +0700

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -9,6 +9,8 @@ module.exports = {
             message.channel.send('here is the main.js file:<https://github.com/arizl/segs/blob/main/main.js>')
         } else if(args[0] === 'commands'){
             message.channel.send('here are the commands .js files:\n<https://github.com/arizl/segs/tree/main/commands>')
+        } else if (!args[0]){
+            message.channel.send('missing component (ex: segs github main)')
         }
     }
 }
