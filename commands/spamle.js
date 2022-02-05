7dcd74266386d7f802f663569222a9bd2877e764
arizl <imariuwu@gmail.com>
2022-02-05 20:12:47 +0700

    done with say, starting spamle.js

---
+++ commands/spamle.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "spamle",
+    description: "help menu",
+    execute(client,message,args,prefix){
+        if (message.author.bot) return;
+        if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")){
+            message.channel.send('mày làm gì có tuổi ping <:lul:806387931606024232>')
+        } else {
+        args.shift()
+
+        if(Number.isNaN(+args)){
+            message.channel.send('that is not a valid amount of times to ping!')
+        } else {
+            for(let i = 0; i < +args; i++){
+                message.channel.send('<@840145281806368798>')
+            }
+        }
+        }
+    }
+}
--- main.js
+++ main.js
@@ -137,26 +137,11 @@ client.on("messageCreate",message=>{
     }
 
     else if(command === 'say'){
-        client.commands.get('say').execute(client,message,args);
+        client.commands.get('say').execute(client, message, args);
     }
 
     else if(command === 'spamle'){
-        if (message.author.bot) return;
-        if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")){
-            message.channel.send('mày làm gì có tuổi ping <:lul:806387931606024232>')
-        } else {
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        console.log(args)
-        args.shift()
-
-        if(Number.isNaN(+args)){
-            message.channel.send('that is not a valid amount of times to ping!')
-        } else {
-            for(let i = 0; i < +args; i++){
-                message.channel.send('<@840145281806368798>')
-            }
-        }
-        }
+        client.commands.get('spamle').execute(client, message, args, prefix);
     }
 });
 
