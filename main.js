f5af6fa112cf8a35a388cfb5e48a0e080e9656d3
arizl <imariuwu@gmail.com>
2022-02-05 17:34:33 +0700

    idk

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ client.on("messageCreate",message=>{
     }
 
     else if(command === 'user'){
-        client.commands.get('user').execute(message, args);
+        client.commands.get('user').execute(message, args, channel);
     }
 
     else if(command === 'lock'){
