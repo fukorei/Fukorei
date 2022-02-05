48dc0dce87402293c7ddb7a00e2e9142d7735836
arizl <imariuwu@gmail.com>
2022-02-05 14:01:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -218,7 +218,7 @@ client.on("messageCreate",message=>{
         if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")) return;
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         args.shift()        
-        if (!args[1]) {
+        if (!args[0]) {
         message.channel.send("what do you want me to say dumbass <:mikewhat:806372341629976586>")
         }
         const say = args.join(" ");
