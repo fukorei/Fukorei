682be6b190b2a5271b20be9474c3dea5601c47c0
arizl <imariuwu@gmail.com>
2022-02-06 10:56:10 +0700

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -5,21 +5,18 @@ module.exports = {
     description: 'basic maths',
     execute(client, message, args) {
         const symbols = ["+", "-", "*", "/"];
-        // args = ["30" , "+", "20","-", "15", "*"]
         
         let total = 0;
         let cSymbol = "+"
         args.foreach(index => {
           if(!isNan(index)){
             switch(cSymbol){
-              // The symbol is + so i have to add
               case "+":{
                 total += Number(index)
                 break;
               }
             }
           }
-          // If its not a number then validate the symbol. and assign it.
           if(symbols.includes(index)) cSymbol = index;
         })
 
