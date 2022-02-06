dbcd3324aa4bdb61d505dff0d5726be2298a7d9c
arizl <imariuwu@gmail.com>
2022-02-06 19:16:01 +0700

    Update cmdswithoutprfx.js

--- commands/cmdswithoutprfx.js
+++ commands/cmdswithoutprfx.js
@@ -42,6 +42,7 @@ module.exports = {
         }
 
         else if(message.content.toLowerCase().startsWith("how much is")){
+            args.shift()
             client.commands.get('maths').execute(client, message, args);
         }
     }
