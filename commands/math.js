fa03a5efb06ae2814da26fb77554bc1e2cc22b46
arizl <imariuwu@gmail.com>
2022-02-06 10:55:52 +0700

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -4,19 +4,25 @@ module.exports = {
     name: 'maths',
     description: 'basic maths',
     execute(client, message, args) {
-        if(Number.isNaN(+args[0]) || Number.isNaN(+args[2])){
-            return message.channel.send('numbers and signs must be seperated!')
-        } else{
-            const maths
-            switch (maths){
-                case plus: 
-                if(!args[1] === "+") return;
-                if(args[1] === "+"){
-                    const result = +args[0] + +args[2]
-                    console.log(result)
-                    message.channel.send(result)
-                }
+        const symbols = ["+", "-", "*", "/"];
+        // args = ["30" , "+", "20","-", "15", "*"]
+        
+        let total = 0;
+        let cSymbol = "+"
+        args.foreach(index => {
+          if(!isNan(index)){
+            switch(cSymbol){
+              // The symbol is + so i have to add
+              case "+":{
+                total += Number(index)
+                break;
+              }
             }
-        }
+          }
+          // If its not a number then validate the symbol. and assign it.
+          if(symbols.includes(index)) cSymbol = index;
+        })
+
+        return message.channel.send(total)
     }
 }
