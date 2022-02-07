c0ea35d1fc5b9854769499d9b96ca0ad243248d3
arizl <imariuwu@gmail.com>
2022-02-07 21:06:25 +0700

    scrapping afk for later

--- commands/afk.js
+++ commands/afk.js
@@ -1,13 +1,13 @@
-const { Collection } = require('discord.js');
-const { afk } = require("../collection");
+// const { Collection } = require('discord.js');
+// const { afk } = require("../collection");
 
-module.exports = {
-    name: 'afk',
-    execute(client, message, args) {
-        const reason = args.join(" ") || 'no reason!';
+// module.exports = {
+//     name: 'afk',
+//     execute(client, message, args) {
+//         const reason = args.join(" ") || 'no reason!';
 
-        afk.set(message.author.id, [Date.now(), reason]);
+//         afk.set(message.author.id, [Date.now(), reason]);
 
-        message.reply(`i set your afk: ${reason}`)
-    }
-}
+//         message.reply(`i set your afk: ${reason}`)
+//     }
+// }
