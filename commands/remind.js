67722c4e2272a81c7beac5b68e268d205e6d4767
arizl <imariuwu@gmail.com>
2022-02-07 23:12:49 +0700

    Update remind.js

--- commands/remind.js
+++ commands/remind.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'remind',
     execute(client, message, args, ms) {
         if (!args) {
-            message.reply("add an amount of time!")
-        } else {
-            console.log(args)
-            const reason = args.slice(1).join(' ');
-            message.reply(`i will remind you in ${args[0]}: ${reason}!`)
-            setTimeout(() => {
-                message.channel.send(`<@${message.author.id}> your reminder for ${reason}!`);
-            }, ms(args[0]))
+            return message.reply("add an amount of time!") 
         }
+        
+        console.log(args)
+        const reason = args.slice(1).join(' ');
+        message.reply(`i will remind you in ${args[0]}: ${reason}!`)
+        setTimeout(() => {
+            message.channel.send(`<@${message.author.id}> your reminder for ${reason}!`);
+        }, ms(args[0]))
     }
 }
