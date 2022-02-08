050da17abf07cf2d547c31a304ddaa6e88a6d7c2
arizl <imariuwu@gmail.com>
2022-02-08 09:18:07 +0700

    LMAOOOOPS

--- commands/scrapremind.js
+++ commands/scrapremind.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'remind',
+    name: 'scrapremind',
     description: 'remind an user sth',
     execute(client, message, args) {
         //["time", "reason", "loop or not"]
