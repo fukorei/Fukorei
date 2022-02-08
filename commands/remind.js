0e8ca31dfd64b9889371bdaee1f6211c06eba9e2
arizl <imariuwu@gmail.com>
2022-02-08 09:04:27 +0700

    Update remind.js

--- commands/remind.js
+++ commands/remind.js
@@ -5,7 +5,7 @@ module.exports = {
             return message.reply("add an amount of time!") 
         } else {
         console.log(args)
-        //const reason = args.slice(1).join(" ");
+        const reason = args.slice(1).join(" ");
         message.reply(`i will remind you in ${args[0]}: ${reason}!`)
         setTimeout(() => {
             message.channel.send(`<@${message.author.id}> your reminder for ${reason}!`);
