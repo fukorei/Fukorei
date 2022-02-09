14c5d2fd52af2a6a25e58871f035dd5ce0ada7d5
arizl <imariuwu@gmail.com>
2022-02-09 21:35:18 +0700

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,14 +3,15 @@ module.exports = {
     execute(client, message, args) {
         const reason = args.join(" ") || 'no reason!';
         const afkreason = new Map();
-        let afkrole = message.member.guild.roles.cache.find(role => role.name === "AFK");
 
         if(message.member.guild.roles.cache.some(role => role.name === "AFK")){
-            message.guild.members.cache.get(message.author.id).roles.remove(afkrole);
+            let role = message.member.guild.roles.cache.find(role => role.name === "AFK");
+            if (role) message.guild.members.cache.get(message.author.id).roles.remove(afkrole);
             message.reply(`oh, you're back, removed your afk!`);
             afkreason.delete(message.author.id);
         } else {
-            message.guild.members.cache.get(message.author.id).roles.add(afkrole);
+            let role = message.member.guild.roles.cache.find(role => role.name === "AFK");
+            if (role) message.guild.members.cache.get(message.author.id).roles.add(afkrole);
             message.reply(`you are now afk: ${reason}!`);
             afkreason.set(message.author.id, reason);
         }
