75aae7a6e4f16dcbdbf0a753453711a8104f10a2
arizl <imariuwu@gmail.com>
2022-02-09 20:37:49 +0700

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -5,8 +5,8 @@ module.exports = {
         const afk = new Map()
 
         if(afk.has(message.author.id)){
-            afk.delete(message.author.id)
-            return message.reply(`oh, youre back, removed your afk!`)    
+            afk.delete(message.author.id);
+            return message.reply(`oh, youre back, removed your afk!`);
         } else {
             afk.set(message.author.id, [Date.now(), reason]);
             return message.reply(`i set your afk: ${reason}`);
