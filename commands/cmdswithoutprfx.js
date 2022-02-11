7e7ebb3f4bfdefc97c562ce895f8f76ac3c36b1a
arizl <imariuwu@gmail.com>
2022-02-11 19:48:14 +0700

    Update cmdswithoutprfx.js

--- commands/cmdswithoutprfx.js
+++ commands/cmdswithoutprfx.js
@@ -83,7 +83,7 @@ module.exports = {
             }
 
             mentioneduser.timeout(ms('5m'));
-            message.channel.send('testing');
+            message.channel.send(`con đã mute ${mentioneduser} trong 5 phút ạ!`);
         }
 
         else if(message.content.toLowerCase() === "con gái ơi"){
