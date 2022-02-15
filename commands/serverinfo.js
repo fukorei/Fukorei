43b9ed2decfc900945c2c49c2952555ff2ddabd5
arizl <imariuwu@gmail.com>
2022-02-15 23:41:36 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,8 +7,8 @@ module.exports = {
         .setColor("#ddbec3")
         .setTitle(`Server info for ${message.guild.name}`)
         .addField("Created at:", `${message.guild.createdAt.toDateString()}`)
-        .addField("You joined at:", `${message.guild.joinedAt.toDateString()}`)
-        .addField("Total members count:", `${message.guild.memberCount}`)
+        .addField("You joined at:", `${message.guild.joinedTimestamp.toDateString()}`)
+        .addField("Total members count:", `${message.guild.memberCount.filter(member => !member.user.bot)}`)
         message.reply({ embeds: [serverinf] });
     }
 }
