80b2e28251a19c7dd251a67fba47bd05154d71d7
arizl <98333495+arizl@users.noreply.github.com>
2022-02-16 00:00:32 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,15 +1,15 @@
 const { MessageEmbed } = require('discord.js');
-//const memcount = client.guilds.get(message.guild.id).members.cache.filter(member => !member.user.bot).size;
 
 module.exports = {
     name: 'serverinf',
     execute(client, message, args){
+        const memcount = client.guilds.get(message.guild.id).members.cache.filter(member => !member.user.bot).size;
         const serverinf = new MessageEmbed()
         .setColor("#ddbec3")
         .setTitle(`Server info for ${message.guild.name}`)
         .addField("Created at:", `${message.guild.createdAt.toDateString()}`)
-        .addField("You joined at:", `${message.guild.joinedTimestamp}`)
-        .addField("Total members count:", `${message.guild.memberCount}`)
+        .addField("You joined at:", `${message.author.joinedAt}`)
+        .addField("Total members count:", `${message.guild.memberCount},${memcount}`)
         message.reply({ embeds: [serverinf] });
     }
 }
