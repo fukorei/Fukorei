dcf06ba35b8682953313ef9d27f2ce9d883dc380
arizl <imariuwu@gmail.com>
2022-02-16 19:59:58 +0700

    Update cmdswithoutprfx.js

--- commands/cmdswithoutprfx.js
+++ commands/cmdswithoutprfx.js
@@ -36,7 +36,7 @@ module.exports = {
         }
     
         var urmom = ["ur mom", "your mom"]
-        if(message.content.toLowerCase().includes(urmom)){
+        if(urmom.some(word => message.content.includes(word))){
             message.channel.send('<:yourmom:938793655781695498>')
         } 
     
