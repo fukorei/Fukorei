28bea8901dcecc5f080ee4cb4da1e8eb6bae9d4e
arizl <imariuwu@gmail.com>
2022-02-18 20:37:32 +0700

    Update remind.js

--- commands/remind.js
+++ commands/remind.js
@@ -1,11 +1,13 @@
 module.exports = {
     name: 'remind',
     execute(client, message, args, ms) {
+        const reason = args.slice(1).join(" ");
+        const time = message.content.slice(3,4)
+        console.log(time)
+
         if (!args[0]) {
             return message.reply("add an amount of time!")
         }
-
-        const reason = args.slice(1).join(" ");
 
         if (!reason) {
             message.reply(`i will remind you in ${args[0]}!`)
