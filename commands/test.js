8c45ab7673eb1e4b74fbeec5448e036508d9ff5b
arizl <imariuwu@gmail.com>
2022-02-20 23:19:40 +0700

    testbench

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'test',
+    description: 'a place to test new commands before release',
+    run: async(client, message, args, moment) => {
+        if(message.content.toLowerCase() === "căng"){
+            message.channel.send(`${message.guild.displayName}\n${message.member.displayName}\n${message.guild.member.displayName}`)
+            } 
+    }
+}
