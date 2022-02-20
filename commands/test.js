b9ce4824d9e9cc95244c363126be982bb81b403d
arizl <imariuwu@gmail.com>
2022-02-20 23:21:31 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,6 +5,5 @@ module.exports = {
     description: 'a place to test new commands before release',
     run: async(client, message, args, moment) => {
             message.channel.send(`${message.guild.displayName}\n${message.member.displayName}\n${message.guild.member.displayName}`)
-            } 
-    }
+        } 
 }
