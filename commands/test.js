6c6abcf30d109f879ae9c328b0e34ebb13a63e52
arizl <imariuwu@gmail.com>
2022-02-21 21:20:34 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -6,11 +6,10 @@ module.exports = {
     name: 'test',
     description: 'a place to test new commands before release',
     run: async(client, message, args, moment) => {
-        let timenow = moment().millisecond(Date.now())
+        moment().millisecond(Date.now())
         message.channel.send(`${timenow}`)
 
-        let twentyfourhours = ms('23h59m')
-        message.channel.send(`${twentyfourhours}`)
+        message.channel.send(`23 hours in ms:${ms('23h')}\n59 minutes in ms:${ms('59m')}\nwhat if i add them both?: ${ms('23h') + ms('59m')}`)
 
 
 }
