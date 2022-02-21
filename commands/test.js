425718e209c45be7926d6d78c6df452c973533b5
arizl <imariuwu@gmail.com>
2022-02-21 15:12:23 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -4,6 +4,10 @@ module.exports = {
     name: 'test',
     description: 'a place to test new commands before release',
     run: async(client, message, args, moment) => {
-            message.channel.send(`${message.member.displayName}`)
+        const test = new Discord.MessageEmbed()
+        .setColor("#ddbec3")
+        .addField("test1", `this is a testbench`)
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+    message.reply({ embeds: [test] });
         } 
 }
