0c3b12d883b0b490ea0e460f8604f4df09b826fe
arizl <imariuwu@gmail.com>
2022-02-21 21:01:01 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,12 +5,12 @@ module.exports = {
     name: 'test',
     description: 'a place to test new commands before release',
     run: async(client, message, args, moment) => {
-        let timenow = moment().milisecond(Date.now())
+        let timenow = moment().millisecond(Date.now())
         message.channel.send(timenow)
 
         let twentyfourhours = ms('23h59m')
         message.channel.send(twentyfourhours)
 
-        
+
 }
 }
