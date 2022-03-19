dc18084463a79469dc1031314939279445232968
arizl <imariuwu@gmail.com>
2022-03-19 11:35:13 +0700

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'announce',
     description: 'announce in the announcements channel',
     run: async (client, message, args) => {
-        const announcechannel = client.channels.cache.find(channel => channel.name === announcements)
+        const announcechannel = client.channels.cache.find(channel => channel.name === "announcements")
         if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")) return;
 
         if(!args.length) return message.channel.send("what do you want me to announce <:mikewhat:806372341629976586>");
