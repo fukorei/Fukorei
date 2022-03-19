b7ef7ed72938d6b7a3d28a3842f449d5050077d8
arizl <imariuwu@gmail.com>
2022-03-19 11:32:34 +0700

    changing a few things

--- commands/test.js
+++ commands/test.js
@@ -7,32 +7,32 @@ module.exports = {
         if(!message.author.id === 732043268946133133) {
             return message.reply ("you shall not have access to this command!");
         }
-        
-        const reason = args.slice(1).join(" ") || "no reason!";
-        const remind = ms(`${args[0]}`);
-        const currenttime = Date.now();
 
-        if (!args[0]) {
-            return message.reply("add an amount of time!")
-        }
-        
-        else if (!reason) {
-            message.reply(`i will remind you in ${args[0]}!`)
-        };
+        // const reason = args.slice(1).join(" ") || "no reason!";
+        // const remind = ms(`${args[0]}`);
+        // const currenttime = Date.now();
 
-        let data;
-            try {
-                data = await schema.create({
-                    userId: message.author.id,
-                    guildId: message.guild.id,
-                    remindreason: `${reason}`,
-                    currenttime: `${currenttime}`,
-                    remindtime: `${currenttime + remind}`
-                })
-            } catch(e) {
-                console.log(e)
-            }
+        // if (!args[0]) {
+        //     return message.reply("add an amount of time!")
+        // }
+        
+        // else if (!reason) {
+        //     message.reply(`i will remind you in ${args[0]}!`)
+        // };
+
+        // let data;
+        //     try {
+        //         data = await schema.create({
+        //             userId: message.author.id,
+        //             guildId: message.guild.id,
+        //             remindreason: `${reason}`,
+        //             currenttime: `${currenttime}`,
+        //             remindtime: `${currenttime + remind}`
+        //         })
+        //     } catch(e) {
+        //         console.log(e)
+        //     }
             
-        message.reply(`i will remind you in ${args[0]}: ${reason}!`)
+        // message.reply(`i will remind you in ${args[0]}: ${reason}!`)
     }
 }
