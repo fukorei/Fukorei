d55e6a267a4ab71a8fb4839ce50d657131d9b43a
arizl <imariuwu@gmail.com>
2022-03-28 17:09:24 +0700

    did an oopsie

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: new Intents(32767) });
 const url = process.env.mongooseConnectionString;
 const db = new Database(url);
-const prefix = ["fukorei ", "f ", "fuko"];
+const prefix = ('f ');
 
 client.commands = new Collection();
 client.emitters = { client };
@@ -45,7 +45,7 @@ client.on("messageCreate", async (message) => {
     const blarr = ["606409312411058176"];
     if (blarr.includes(message.author.id)) return;
 
-    if(!prefix.find(p=>message.content.toLowerCase().startsWith(p))) {
+    if (!message.content.toLowerCase().startsWith(prefix)) {
         client.commands.get('cmdswithoutprfx').run(client, message, args, ms, afks);
     } else {
         client.commands.get('cmds').run(client, message, args, prefix, ms, command, moment, url, db);
