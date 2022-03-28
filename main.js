7e64897d04f13efabeb860af3fec65d38a741777
arizl <imariuwu@gmail.com>
2022-03-28 22:49:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ client.on("messageCreate", async (message) => {
     const blarr = ["606409312411058176"];
     if (blarr.includes(message.author.id)) return;
 
-    if(!prefix.includes(message.content.toLowerCase())) {
+    if(prefix.includes(message.content.toLowerCase()) == false) {
         client.commands.get('cmdswithoutprfx').run(client, message, args, ms, afks);
     } else {
         client.commands.get('cmds').run(client, message, args, prefix, ms, command, moment, url, db);
