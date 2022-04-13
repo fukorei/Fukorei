173283cfe580d58ae4c540fd92d940a5020108fe
arizl <imariuwu@gmail.com>
2022-04-13 21:42:28 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,6 +3,9 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'serverinf',
     run: async(client, message, args, moment) => {
+        let botcount=0;
+        message.guild.members.cache.map(m=>{if(m.user.bot) botcount++})
+
         //const memcount = client.guilds.get(message.guild.id).members.cache.filter(member => !member.user.bot).size;
         const serverinf = new MessageEmbed()
         .setColor("#ddbec3")
@@ -10,7 +13,7 @@ module.exports = {
         .addField("created at:", `${moment(message.guild.createdAt).format('ddd, DD/MM/YYYY')}`)
         .addField("you joined the server at:", `${moment(message.member.joinedAt).format('ddd, DD/MM/YYYY')}`)
         .addField("you joined discord at:", `${moment(message.author.createdAt).format('ddd, DD/MM/YYYY')}`)
-        .addField("Total members count:", `${message.guild.memberCount}`)
+        .addField("Total members count:", `${message.guild.memberCount-botcount} (${botcount} bots)`)
         .setFooter({
             text: `requested by ${message.author?.username}#${message.author?.discriminator}`,
             iconURL: message.member.displayAvatarURL()
