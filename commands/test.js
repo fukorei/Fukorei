770a24008224661f1c7fba4f96ae8ac309ac42a0
arizl <imariuwu@gmail.com>
2022-04-13 20:44:33 +0700

    test

--- commands/test.js
+++ commands/test.js
@@ -8,33 +8,11 @@ module.exports = {
             return message.reply ("you shall not have access to this command!");
         }
 
-        message.reply(`${message.guild.createdAt}`);
+        const lg1 = new MessageEmbed()
+        .setColor("#ddbec3")
+        .setTitle("cos đối - sin bù - phụ chéo - khác pi tan")
+        .setImage("https://media.discordapp.net/attachments/606112569622659076/963795875744866354/dmlg1.png?width=319&height=405")
 
-        // const reason = args.slice(1).join(" ") || "no reason!";
-        // const remind = ms(`${args[0]}`);
-        // const currenttime = Date.now();
-
-        // if (!args[0]) {
-        //     return message.reply("add an amount of time!")
-        // }
-        
-        // else if (!reason) {
-        //     message.reply(`i will remind you in ${args[0]}!`)
-        // };
-
-        // let data;
-        //     try {
-        //         data = await schema.create({
-        //             userId: message.author.id,
-        //             guildId: message.guild.id,
-        //             remindreason: `${reason}`,
-        //             currenttime: `${currenttime}`,
-        //             remindtime: `${currenttime + remind}`
-        //         })
-        //     } catch(e) {
-        //         console.log(e)
-        //     }
-            
-        // message.reply(`i will remind you in ${args[0]}: ${reason}!`)
+        message.reply({ embeds: [lg1] });
     }
 }
