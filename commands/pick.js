36256fd5e33b21b3c21bfcda37851dc11d1f6e2c
arizl <imariuwu@gmail.com>
2022-06-09 09:04:26 +0700

    Update pick.js

--- commands/pick.js
+++ commands/pick.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'pick',
     run: async (message, args) => {
     if(!args) return message.reply ("you must provide arguments!");
-
+    console.log(args);
     message.reply(`i choose `+`${Math.floor(Math.random() * args.length)}`);
     }
 }
