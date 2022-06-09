e8c16cfb3de0b1b3d298171fe83fc252f9b449fa
arizl <imariuwu@gmail.com>
2022-06-09 09:27:06 +0700

    Update pick.js

--- commands/pick.js
+++ commands/pick.js
@@ -5,7 +5,12 @@ module.exports = {
 
     console.log(args);
     let pickargs = message.content;
-    
+    for (let i = 0; i < args.length; i++) {
+        if(pickargs[i]==="|"){
+            pickargs.split
+        }
+    }
+    console.log(pickargs);
     const pick = args[Math.floor(Math.random() * args.length)];
     message.reply(`i choose `+`${pick}`);
     }
