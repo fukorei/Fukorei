9b1bcc9a0cd06855d604776811425022b7f53b9e
arizl <imariuwu@gmail.com>
2022-06-17 21:39:22 +0700

    Update developer.js

--- commands/developer.js
+++ commands/developer.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'secret',
+    name: 'developer',
     run: async(client, message, args) => {
     if(message.author.id !== "732043268946133133") return message.reply("you are not the bot dev!");
-    
+    console.log("developer args:" + args);
     if(args[0] === "spameveryone"){
         if(message.channel.id !== "921394718376611850") return message.reply("no");
         for(let i = 0; i<=10; i++){
