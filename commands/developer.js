d6dd7750bba1f6ef0f7fd91c5b466b1eadff04ca
arizl <imariuwu@gmail.com>
2022-06-19 22:06:00 +0700

    Update developer.js

--- commands/developer.js
+++ commands/developer.js
@@ -3,9 +3,11 @@ module.exports = {
     run: async(client, message, args) => {
     if(message.author.id !== "732043268946133133") return message.reply("you are not the bot dev!");
 
+    if(!args[0]) return message.reply("missing arguments.")
+    
     if(args[0] === "spam.everyone"){
         if(message.channel.id !== "921394718376611850") return message.reply("no");
-        for(let i = 0; i<=5; i++){
+        for(let i = 0; i<5; i++){
             message.channel.send("@everyone")
         }
     }
