e8745d719f2c4f8babbd19b19143f0b02b2fd2f9
arizl <imariuwu@gmail.com>
2022-06-20 15:17:14 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,6 +5,7 @@ module.exports = {
         let MessageCache = new Map();
         client.MessageDeleteCache = MessageCache;
         client.on("messageDelete",(msg) => MessageDeleteCache.set(message.channel.id,{msg,deleteUser: deleteUserFromEvent}))
-        return message.channel.send(client.MessageDeleteCache.get(message.channel.id))
+        let msg = client.MessageDeleteCache.get(message.channel.id)
+        return message.channel.send(msg)
     }
 }
