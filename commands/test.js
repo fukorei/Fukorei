5f2b378469caedeadbea4e6920105aa4f8797da5
arizl <imariuwu@gmail.com>
2022-06-20 15:15:34 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,20 +1,10 @@
-const discord = require('discord.js')
-const schema = require('../schema/afkschema')
-const { MessageEmbed } = require('discord.js');
-const { pagination } = require('reconlx');
-const Map = require('discord.js');
 
 module.exports = {
     name: 'test',
     run: async(client, message, args, ms) => {
-        // message.reply("here lies new features i guess i dont fucking know how to code")
-
         let MessageCache = new Map();
-        client.on("messageDelete",(msg) => {
-            MessageCache.set(msg.guildId,msg)
-        })
-
-        let msg = MessageCache.get(message.guildId)
-        message.reply(msg)
+        client.MessageDeleteCache = MessageCache;
+        client.on("messageDelete",(msg) => MessageDeleteCache.set(message.channel.id,{msg,deleteUser: deleteUserFromEvent}))
+        return message.channel.send(client.MessageDeleteCache.get(message.channel.id))
     }
 }
