041261df29b46ef7634f3e54262f271c1ffbb66f
arizl <imariuwu@gmail.com>
2022-06-25 21:18:16 +0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,11 +2,6 @@
 module.exports = {
     name: 'test',
     run: async(client, message, args, ms) => {
-        let MessageCache = new Map();
-        client.MessageDeleteCache = MessageCache;
-        client.on("messageDelete",(msg) => MessageDeleteCache.set(message.channel.id,{msg,deleteUser: deleteUserFromEvent}))
-        return message.channel.send({
-            content: `${client.MessageDeleteCache.get(message.channel.id)}`
-        })
+        message.reply("here lies commands and shit")
     }
 }
