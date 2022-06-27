419227b8bcc605841b33ad027a2dc77cc63699b3
arizl <imariuwu@gmail.com>
2022-06-27 22:43:21 +0700

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -3,13 +3,11 @@ module.exports = {
     run: async(client, message, args) => {
         let ping = message.mentions.users.first();
         if(message.author.bot) return;
-        if(!message.member.permissionsIn(message.channel).has("ADMINISTRATOR") || message.author !== ping)
-            return message.reply('mày làm gì có tuổi ping <:lul:806387931606024232>')
 
         if (Number.isNaN(+args[0]))
             return message.reply('that is not a valid amount of times to ping!')
 
-        if (+args[0] > 25)
+        if (+args[0] > 5)
             return message.reply('listen man im hosted on heroku stop tf is wrong with you <:mikewhat:806372341629976586> ')
 
         for (let i = 0; i < +args[0]; i++)
