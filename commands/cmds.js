415f83041a70d4b31c320bd8f809b0663598f504
arizl <imariuwu@gmail.com>
2023-02-12 11:24:58 +0700

    where boost mikewhat

--- commands/cmds.js
+++ commands/cmds.js
@@ -82,12 +82,7 @@ module.exports = {
 
         else if(command === 'announce'){
             client.commands.get('announce').run(client, message, args);
-        }
-
-        const lg = ["luonggiac", "lg"]
-        if(lg.includes(command)){
-            client.commands.get('luonggiac').run(message, args);
-        }
+        } 
 
         else if(command === 'setname'){
             client.commands.get('setname').run(message, args);
