ac6effcb00103f4c990aa48cd860446b86682cc3
arizl <imariuwu@gmail.com>
2023-04-01 20:15:53 +0700

    updating a few things trying different models and messing around

--- commands/gpt.js
+++ commands/gpt.js
@@ -10,16 +10,17 @@ module.exports = {
         const prompt = message.content.substring(6)
 
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: "gpt-3.5-turbo",
             prompt,
             temperature: 0.7,
-            max_tokens: 256,
+            max_tokens: 2000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
         });
 
     const answer = response.data.choices[0].text;
+    console.log(response.data)
     message.reply(answer);
     }
 };
